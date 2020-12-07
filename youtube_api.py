f0c88e4c620725fd76f0fbf00110db5e253576ab
Kerem Sanyilmaz <keremsanyilmaz@gmail.com>
2020-12-07 17:48:30 +0300

    adde data file

--- youtube_api.py
+++ youtube_api.py
@@ -1,10 +1,13 @@
 from googleapiclient.discovery import build
 
-youtube_api_key = ''
+import time
+import json
+
+youtube_api_key = 'AIzaSyDuD7QzJKawbnhkupYvbn7dievqf_Yqr6k'
 youtube = build('youtube', 'v3', developerKey=youtube_api_key)
 
-from_date = '2020-04-01T00:00:00Z'
-to_date = '2020-05-01T00:00:00Z'
+from_date = '2019-03-01T00:00:00Z'
+to_date = '2019-11-30T00:00:00Z'
 search_api = youtube.search()
 videos_api = youtube.videos()
 video_categories_api = youtube.videoCategories()
@@ -27,7 +30,7 @@ def get_categories():
     return categories
 
 
-def search_videos(query_string, from_date, to_date, location, location_radius, pageToken=None):
+def search_videos(query_string, from_date, to_date, location, radius, page_token=None):
     list_parameters = {
         'part': 'snippet',
         'type': 'video',
@@ -39,10 +42,34 @@ def search_videos(query_string, from_date, to_date, location, location_radius, p
         'publishedAfter': from_date,
         'publishedBefore': to_date,
     }
+    if page_token:
+        list_parameters['pageToken'] = page_token
 
     videos = search_api.list(**list_parameters).execute()
 
     return videos
+
+
+def search_all_videos(query_string, from_date, to_date, location, radius, page_token=None):
+    all_search_data = []
+    for i in range(90):
+        search_data = search_videos(
+            query_string,
+            from_date,
+            to_date,
+            location,
+            radius,
+            page_token=page_token
+        )
+
+        print(page_token, search_data['pageInfo'], len(search_data['items']))
+        all_search_data.append(search_data)
+
+        page_token = search_data.get('nextPageToken')
+        if not page_token or len(search_data['items']) == 0:
+            return all_search_data
+
+    return all_search_data
 
 
 def get_video_ids_from_search_data(search_data):
@@ -65,6 +92,15 @@ def get_videos_from_search_data(search_data):
     return videos
 
 
+def get_videos_from_search_data_list(search_datas):
+    all_videos = []
+    for search_data in search_datas:
+        videos = get_videos_from_search_data(search_data)
+        all_videos.extend(videos['items'])
+
+    return all_videos
+
+
 def normalize_videos_data(videos):
     categories = get_categories()
 
@@ -74,3 +110,11 @@ def normalize_videos_data(videos):
         video['snippet']['categoryName'] = category_name
 
     return videos
+
+
+def read_data_from_file(path):
+    data_file = open(path, 'r')
+    data_txt = data_file.read()
+    data = json.loads(data_txt)
+
+    return data
